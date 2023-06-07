64c95bc1104627f7b315a04dc16e3764fcf2eb9a
Biggs <48188031+WarEagleBiggs@users.noreply.github.com>
2023-06-07 16:31:52 -0500

    Added basic plane movement code

---
+++ Assets/PlaneInterp.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneInterp : MonoBehaviour
+{
+     public Transform startPos;  // Starting position
+        public Transform endPos;    // Ending position
+        public float speed = 5f;    // Movement speed
+    
+        private float startTime;
+        private float journeyLength;
+    
+        private void Start()
+        {
+            startTime = Time.time;
+            journeyLength = Vector3.Distance(startPos.position, endPos.position);
+        }
+    
+        private void Update()
+        {
+            float distanceCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distanceCovered / journeyLength;
+    
+            transform.position = Vector3.Lerp(startPos.position, endPos.position, fractionOfJourney);
+    
+            if (fractionOfJourney >= 1f)
+            {
+                Destroy(gameObject);
+            }
+        }
+}
